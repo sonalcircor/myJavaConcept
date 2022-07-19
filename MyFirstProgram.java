38524e3296547cb0c512748addbf32178a93d58b
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-19 23:17:26 +0100

    first java program

---
+++ MyFirstProgram.java
@@ -0,0 +1,15 @@
+package JavaConcepts;
+
+public class MyFirstProgram
+{
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Sonal");
+        System.out.println("I live in london");
+        System.out.println("Im 38yrs old");
+        System.out.println("I live with my family");
+
+
+    }
+
+}
---
+++ unary.java
@@ -0,0 +1,15 @@
+package JavaConcepts;
+
+public class unary {
+    public static void main(String[] args) {
+        short s=101;
+        boolean c = false;
+
+        System.out.println(s++);
+        System.out.println(++s);
+        System.out.println(s--);
+        System.out.println(--s);
+        System.out.println(!c);
+
+    }
+}
